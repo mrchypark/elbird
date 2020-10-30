12730fb44a44cfce4a2a245dd2a0981001c8dfed
chanyub Park <mrchypark@gmail.com>
2020-10-31 05:13:21 +0900

    init version

--- R/el.R
+++ R/el.R
@@ -8,18 +8,56 @@
 add_user_word <- function(word, pos, score){
   el <- get("el", envir = .el)
   el$add_user_word(word, pos, score)
-  el$prepare()
   assign("el", el, envir = .el)
 }
 
+#' analyze
+#'
+#' @param text target text.
+#' @param top_n Number of result. default is 1.
+#'
+#' @name analyze
 #' @importFrom purrr map
 #' @export
 analyze <- function(text, top_n = 1) {
   el <- get("el", envir = .el)
+  el$prepare()
   top_n <- as.integer(top_n)
-  purrr::map(
-    text, ~ el$analyze(.x, top_n)
-  ) -> res
+  res <- purrr::map(
+    text, ~ el$async_analyze(.x, top_n)()[[1]][[1]]
+  )
   return(res)
 }
 
+#' @export
+#' @rdname analyze
+#' @return a [tibble][tibble::tibble-package]
+analyze_tbl <- function(text, top_n = 1) {
+  res <- analyze(text, top_n = 1)
+  res <- purrr::map(
+    res, ~  tibble(
+      morph = purrr::map_chr(.x, ~.x[[1]]),
+      tag = purrr::map_chr(.x, ~.x[[2]]),
+      start = purrr::map_int(.x, ~.x[[3]]),
+      end = purrr::map_int(.x, ~.x[[4]]),
+    )
+  )
+  return(res)
+}
+
+
+#' @export
+#' @rdname analyze
+analyze_tidytext <- function(text, top_n = 1) {
+  res <- analyze_tbl(text, top_n = 1)
+  res <- purrr::map(
+    res, ~
+      paste0(.x$morph, "/",.x$tag)
+  )
+  return(res)
+}
+
+
+#' @export
+#' @rdname analyze
+analyze_tt <- analyze_tidytext
--- R/onLoad.R
+++ R/onLoad.R
@@ -6,23 +6,23 @@
   set_env()
   pak <- reticulate::import("kiwipiepy")
   el <- pak$Kiwi()
-  el$prepare()
   assign("el", el, envir = .el)
 }
 
-#' @export
-#' @importFrom reticulate import import_builtins py_module_available use_condaenv conda_create conda_install conda_list
+#' @importFrom reticulate conda_install
 install_conda_packages <- function() {
   reticulate::conda_install("r-Elbird",
                             pip = TRUE,
                             packages = c("kiwipiepy==0.8.2"))
   cat("\nInstallation complete.\n\n")
 }
-#' @export
+
+#' @importFrom reticulate py_module_available
 check_env <- function() {
   reticulate::py_module_available("kiwipiepy")
 }
-#' @export
+
+#' @importFrom reticulate use_condaenv
 check_conda_set <- function() {
   envnm <- "r-Elbird"
   chk <- try(reticulate::use_condaenv(envnm, required = TRUE), silent = T)
@@ -35,7 +35,6 @@ check_conda_set <- function() {
 }
 
 #' @importFrom reticulate conda_create
-#' @export
 set_env <- function() {
   if (!check_conda_set()) {
     reticulate::conda_create("r-Elbird", packages = "python=3.7")
