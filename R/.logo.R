f192517f07f382fab9856ed290f66e6bf7c00ae3
chanyub Park <mrchypark@gmail.com>
2020-11-01 21:14:49 +0900

    add logo

---
+++ R/.logo.R
@@ -0,0 +1,17 @@
+library(hexSticker)
+
+# for windows
+sticker(
+  "man/figures/moa.png",
+  s_x=1.4, s_y=0.85, s_width = 0.3,
+  package = "Elbird",
+  p_size = 55, p_y = 1.3, p_x = 0.85,
+  filename = "man/figures/logo.png",
+  h_fill = "#FFFFFF",
+  p_color = "#333333",
+  h_color = "#999999",
+  url = "mrchypark.github.io/Elbird",
+  u_size = 9,
+  u_color = "#333333",
+  dpi = 500
+)
