31b1f20739152111ae50915efa1fe73d495f08d1
Chanyub.Park <mrchypark@gmail.com>
2022-03-18 14:29:47 +0000

    lint
    Former-commit-id: b00feac7c6d2c6aea6b11e29bb6eba49cca04228 [formerly b00feac7c6d2c6aea6b11e29bb6eba49cca04228 [formerly 5e064472d8c0af2968a404f7d567f840be53855b]]
    Former-commit-id: 3669f00116aeb16f8e2e4c277dc312614b9b3cbf
    Former-commit-id: 806ac3ce73435be56498995252f176cf0bf54178
    Former-commit-id: 01e4a466cf61f6bd428c80873eb57e016f43751d

--- R/analyze.R
+++ R/analyze.R
@@ -2,10 +2,11 @@
 #'
 #' @param text target text.
 #' @param top_n Number of result. default is 3.
-#'
 #' @name analyze
+#' @export
 analyze <- function(text, top_n = 3) {
   if (init_chk_not()) init()
-
-  return(res)
+  kb <- get("kb", envir = .el)
+  top_n <- as.integer(top_n)
+  return(kiwi_analyze(kb, text, top_n, 1))
 }
