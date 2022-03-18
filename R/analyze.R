97a2e9fc66aded3444f2ad10c5b877c794f64081
Chanyub.Park <mrchypark@gmail.com>
2022-03-18 14:11:29 +0000

    work version
    TODO: named list
    
    Former-commit-id: b07c2c118fdebeb1266ac1c82d0099b80dc3be15 [formerly b07c2c118fdebeb1266ac1c82d0099b80dc3be15 [formerly c36f30099ffdad41e33f2777390001d6fa7901e6]]
    Former-commit-id: 84ed8660f0652a91e0ce96e5791310ae899540e3
    Former-commit-id: 71e8de490b2ea8a35fa62414ab5676a90d4a3317
    Former-commit-id: 8a60f57d47c5722c83030d973e15e263761012ea

--- R/analyze.R
+++ R/analyze.R
@@ -4,24 +4,8 @@
 #' @param top_n Number of result. default is 3.
 #'
 #' @name analyze
-#' @importFrom purrr map
-#' @importFrom tibble tibble
-#' @export
 analyze <- function(text, top_n = 3) {
   if (init_chk_not()) init()
-  el <- get("el", envir = .el)
-  top_n <- as.integer(top_n)
-  iter <- el$analyze(text, top_n)
-  if (is.list(iter)){
-    res <- iter
-  } else {
-    res <- c()
-    while (TRUE) {
-      item <- reticulate::iter_next(iter, completed = NA)
-      if (is.na(item))
-        break
-      res <- c(res, item)
-    }
-  }
+
   return(res)
 }
