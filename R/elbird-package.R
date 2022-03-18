2ca38cc4abbfca162ea06ff44cc4b88ef6e1d9d6
Chanyub.Park <mrchypark@gmail.com>
2022-03-18 21:54:09 +0000

    working default version

--- R/elbird-package.R
+++ R/elbird-package.R
@@ -4,12 +4,7 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
-# @useDynLib elbird, .registration = TRUE
+#' @useDynLib elbird, .registration = TRUE
 #' @importFrom tibble tibble
 ## usethis namespace: end
 NULL
-
-
-# https://github.com/r-lib/devtools/issues/2027#issuecomment-483691800
-# devtools::clean_dll()
-# @useDynLib elbird, .registration = TRUE
--- R/init.R
+++ R/init.R
@@ -2,6 +2,7 @@ init_chk_not <- function() {
   length(ls(envir=.el)) != 1
 }
 
+#' @importFrom methods new
 init <- function() {
   kb <- kiwi_init(model_path_full(), 0, 0)
   if (identical(kb, new("externalptr"))) {
