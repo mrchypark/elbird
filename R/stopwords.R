5941d3d3542b989a37532b70915c8dbe5270b9df
Chanyub.Park <mrchypark@gmail.com>
2022-03-27 10:41:49 +0000

    save user dict

--- R/stopwords.R
+++ R/stopwords.R
@@ -1,5 +1,3 @@
-Tags <- NULL
-
 Stopwords <- R6::R6Class(
   'Stopwords',
   public = list(
@@ -18,40 +16,51 @@ Stopwords <- R6::R6Class(
       invisible(self)
     },
 
-    set_user_dict = function(path, dict_name = "user") {
-      path <- normalizePath(path, mustWork = TRUE)
-      private$set_dict(path)
-      invisible(self)
-    },
-
     add = function(form, tag = "NNP") {
       private$add_dict(tibble::tibble(form = form, tag = check_tag(tag)),
                        "addfunc",
                        paste0(form, "/" , tag))
       invisible(self)
     },
+
+    add_from_dict = function(path, dict_name = "user") {
+      path <- normalizePath(path, mustWork = TRUE)
+      private$set_dict(path)
+      invisible(self)
+    },
+
     remove = function(form = NULL, tag = NULL) {
       word <- tibble::tibble(form = form, tag = check_tag(tag))
       private$stopword_list <-
         dplyr::anti_join(private$stopword_list, word, by = names(word))
       invisible(self)
     },
+
     save_user_dict = function(path) {
+      vroom::vroom_write(
+        x = private$stopword_list,
+        file = path,
+        delim = "/",
+        na = "",
+        col_names = FALSE
+      )
       invisible(self)
     },
+
     get = function() {
       private$stopword_list
     },
+
     use = function() {
       unique(private$stopword_list[c("form", "tag")])
     },
+
     lists = function() {
       private$dict_list
     }
   ),
 
   private = list(
-
     # stopword_list = NULL,
     # init_stopword_list = function() {
     #   private$stopword_list <- tibble::tibble(form = "",
@@ -63,9 +72,11 @@ Stopwords <- R6::R6Class(
 
     dict_list = tibble::tibble(dict_name = character(),
                                info = character()),
+
     set_system_dict = function() {
       private$set_dict(dict_stopwords_path(), "system")
     },
+
     set_dict = function(dict_path, dict_name) {
       loaded <-
         vroom::vroom(
@@ -76,24 +87,18 @@ Stopwords <- R6::R6Class(
         )
       loaded <- unique(loaded)
       names(loaded) <- c("form", "tag")
-      private$add_dict(dplyr::mutate(
-                        loaded,
-                        tag = purrr::map(tag, ~ check_tag(.x))[[1]]
-                        ),
+      private$add_dict(dplyr::mutate(loaded,
+                                     tag = purrr::map_chr(tag, ~ check_tag(.x))),
                        dict_name,
                        dict_path)
     },
+
     add_dict = function(dict, dict_name, dict_info) {
       private$stopword_list <-
         dplyr::bind_rows(private$stopword_list, dict)
       private$dict_list <-
         dplyr::bind_rows(private$dict_list,
                          tibble::tibble(dict_name = dict_name, info = dict_info))
-    },
-    validated = function(data) {
-      if (is.vector(data) && is.character(data)) {
-
-      }
     }
   )
 )
