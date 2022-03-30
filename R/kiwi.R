085e13ad5bbf3e96503e6da8f91953b771dd1197
Chanyub.Park <mrchypark@gmail.com>
2022-03-30 04:55:42 +0000

    kiwi docs

--- R/kiwi.R
+++ R/kiwi.R
@@ -1,6 +1,13 @@
-#' kiwi R6 class
+#' kiwi class
+#'
+#' @description
+#'   kiwi class is provide method for korean mophological analyze result.
 #'
 #' @importFrom R6 R6Class
+#' @examples
+#'   kw <- Kiwi$new()
+#'   kw$analyze("test")
+#'   kw$tokenize("test")
 #' @export
 Kiwi <- R6::R6Class(
   "Kiwi",
@@ -16,10 +23,10 @@ Kiwi <- R6::R6Class(
 
     #' @description
     #'   Create a kiwi instance.
-    #' @param num_workers use multi-thread core number. default is 0 which means use all core.
-    #' @param model_size kiwi model select. default is "base". "small", "large" is available.
-    #' @param integrate_allomorph default is TRUE.
-    #' @param load_default_dict use defualt dictionary. default is TRUE.
+    #' @param num_workers \code{int(optional)}: use multi-thread core number. default is 0 which means use all core.
+    #' @param model_size \code{char(optional)}: kiwi model select. default is "base". "small", "large" is available.
+    #' @param integrate_allomorph \code{bool(optional)}: default is TRUE.
+    #' @param load_default_dict \code{bool(optional)}: use defualt dictionary. default is TRUE.
     initialize = function(num_workers = 0,
                           model_size = "base",
                           integrate_allomorph = TRUE,
@@ -43,10 +50,9 @@ Kiwi <- R6::R6Class(
 
     #' @description
     #'   add user word with pos and score
-    #' @param word target word to add.
-    #' @param pos pos information about word.
-    #' @param score score information about word.
-    #' @return
+    #' @param word \code{char(required)}: target word to add.
+    #' @param pos \code{char(required)}: pos information about word.
+    #' @param score \code{num(required)}: score information about word.
     add_user_words = function(word, pos, score) {
       kiwi_builder_add_word_(private$kiwi_builder, word, pos, score)
     },
@@ -60,7 +66,7 @@ Kiwi <- R6::R6Class(
 
     #' @description
     #'   add user dictionary using text file.
-    #' @param user_dict_path path of user dictionary file.
+    #' @param user_dict_path \code{char(required)}: path of user dictionary file.
     load_user_dictionarys = function(user_dict_path) {
       # TODO validate dict
       # TODO add user dict list for save
@@ -69,12 +75,11 @@ Kiwi <- R6::R6Class(
 
     #' @description
     #'   Extract Noun word candidate from texts.
-    #' @param input target text data
-    #' @param min_cnt minimum count of word in text.
-    #' @param max_word_len max word length.
-    #' @param min_score minimum score.
-    #' @param pos_threshold pos threashold.
-    #' @return
+    #' @param input \code{char(required)}: target text data
+    #' @param min_cnt \code{int(required)}: minimum count of word in text.
+    #' @param max_word_len \code{int(required)}: max word length.
+    #' @param min_score \code{num(required)}: minimum score.
+    #' @param pos_threshold \code{num(required)}: pos threashold.
     extract_words =  function(input,
                               min_cnt,
                               max_word_len,
@@ -90,12 +95,11 @@ Kiwi <- R6::R6Class(
 
     #' @description
     #'   Extract Noun word candidate from texts and add user words.
-    #' @param input target text data
-    #' @param min_cnt minimum count of word in text.
-    #' @param max_word_len max word length.
-    #' @param min_score minimum score.
-    #' @param pos_threshold pos threashold.
-    #' @return
+    #' @param input \code{char(required)}: target text data
+    #' @param min_cnt \code{int(required)}: minimum count of word in text.
+    #' @param max_word_len \code{int(required)}: max word length.
+    #' @param min_score \code{num(required)}: minimum score.
+    #' @param pos_threshold \code{num(required)}: pos threashold.
     extract_add_words =  function(input,
                                   min_cnt,
                                   max_word_len,
@@ -111,16 +115,15 @@ Kiwi <- R6::R6Class(
 
     #' @description
     #'   Analyze text to token and pos results.
-    #' @param text target text.
-    #' @param top_n number of result. Default is 3.
-    #' @param match_option [`Match`]: use Match. Default is Match$ALL
-    #' @param stopwords stopwords option. Default is TRUE which is
-    #'                  to use embaded stopwords dictionany.
-    #'                  If FALSE, use not embaded stopwords dictionany.
-    #'                  If char: path of dictionary txt file, use file.
+    #' @param text \code{char(required)}: target text.
+    #' @param top_n \code{int(optional)}: number of result. Default is 3.
+    #' @param match_option match_option [`Match`]: use Match. Default is Match$ALL
+    #' @param stopwords stopwords option. Default is FALSE which is use nothing.
+    #'                  If \code{TRUE}, use embaded stopwords dictionany.
+    #'                  If \code{char}: path of dictionary txt file, use file.
     #'                  If [`Stopwords`] class, use it.
     #'                  If not valid value, work same as FALSE.
-    #' @return
+    #' @return \code{list} of result.
     analyze = function(text,
                        top_n = 3,
                        match_option = Match$ALL,
@@ -133,16 +136,15 @@ Kiwi <- R6::R6Class(
 
     #' @description
     #'   Analyze text to token and pos result just top 1.
-    #' @param text target text.
-    #' @param match_option [`Match`]: use Match. Default is Match$ALL
-    #' @param stopwords stopwords option. Default is TRUE which is
-    #'                  to use embaded stopwords dictionary.
-    #'                  If FALSE, use not embaded stopwords dictionary.
-    #'                  If char: path of dictionary txt file, use file.
+    #' @param text \code{char(required)}: target text.
+    #' @param match_option match_option [`Match`]: use Match. Default is Match$ALL
+    #' @param stopwords stopwords option. Default is FALSE which is use nothing.
+    #'                  If \code{TRUE}, use embaded stopwords dictionany.
+    #'                  If \code{char}: path of dictionary txt file, use file.
     #'                  If [`Stopwords`] class, use it.
     #'                  If not valid value, work same as FALSE.
-    #' @param form return form. default is "tibble". "list", "tidytext" is available.
-    #' @return
+    #' @param form \code{char(optional)}: return form. default is "tibble".
+    #'                                     "list", "tidytext" is available.
     tokenize = function(text,
                         match_option = Match$ALL,
                         stopwords = FALSE,
@@ -183,7 +185,11 @@ Kiwi <- R6::R6Class(
     #'                  If char: path of dictionary txt file, use file.
     #'                  If [`Stopwords`] class, use it.
     #'                  If not valid value, work same as FALSE.
-    #' @return
+    #' @examples
+    #'    kw <- Kiwi$new()
+    #'    tidytoken <- kw$get_tidytext_func()
+    #'    tidytoken("test")
+    #' @return \code{function}
     get_tidytext_func = function(match_option = Match$ALL,
                                  stopwords = FALSE) {
       function(text) {
@@ -198,10 +204,9 @@ Kiwi <- R6::R6Class(
     #' Some text may not split sentence by sentence.
     #' split_into_sents works split sentences to sentence by sentence.
     #'
-    #' @param text target text.
+    #' @param text \code{char(required)}: target text.
     #' @param match_option match_option [`Match`]: use Match. Default is Match$ALL
-    #' @param return_tokens add tokenized resault.
-    #' @return
+    #' @param return_tokens \code{bool(optional)}: add tokenized resault.
     split_into_sents = function(text,
                                 match_option = Match$ALL,
                                 return_tokens = FALSE) {
