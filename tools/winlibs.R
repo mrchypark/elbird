ae6f0ba40697f9a7d18fd14c597bf11a0ab75639
Chanyub.Park <mrchypark@gmail.com>
2022-03-30 08:01:05 +0000

    add tools winlibs.R and modify for windows Makevars

---
+++ tools/winlibs.R
@@ -0,0 +1,9 @@
+if (!file.exists("../windows/rwinlib-elbird-0.11.0/include/kiwi/capi.h")) {
+
+  if (getRversion() < "3.3.0") setInternet2()
+  download.file("https://github.com/mrchypark/rwinlib-elbird/archive/refs/tags/v0.11.0.zip", destfile = "kiwi-release.zip", quiet = TRUE)
+  dir.create("../windows", showWarnings = FALSE)
+  unzip("kiwi-release.zip", exdir = "../windows")
+  unlink("kiwi-release.zip")
+
+}
