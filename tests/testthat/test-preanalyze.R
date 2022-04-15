68516fecbfde6ab265ffe0c90aaef0c3c6432214
Chanyub.park <mrchypark@gmail.com>
2022-04-15 11:17:40 +0900

    lint test case

--- tests/testthat/test-preanalyze.R
+++ tests/testthat/test-preanalyze.R
@@ -3,34 +3,6 @@ test_that("pre analyze words df", {
   if (!model_works("small"))
     get_model("small")
 
-  anl <- data.frame(
-    morphs = c("팅기", "었", "어"),
-    pos = c("vv", "ep", "ef"),
-    start = c(0L, 1L, 2L),
-    end = c(1L, 2L, 3L)
-  )
-
-  kb <- kiwi_builder_init_(kiwi_model_path_full("small"), 0, BuildOpt$DEFAULT)
-
-  res <- kiwi_builder_add_pre_analyzed_word_(kb, "팅겼어", anl, 0)
-  expect_equal(res, -1)
-
-  kiwi_builder_add_alias_word_(kb, "팅기","vv", -1, "튕기")
-
-  res <- kiwi_builder_add_pre_analyzed_word_(kb, "팅겼어", anl, 0)
-  expect_equal(res, 0)
-  kw <- kiwi_builder_build_(kb)
-  res <- kiwi_analyze_wrap(kw, text = "팅겼어...", 1, Match$ALL_WITH_NORMALIZING)
-  expect_equal(res[[1]]$Token[[1]]$form, "팅기")
-  expect_equal(res[[1]]$Token[[1]]$tag, "VV")
-})
-
-
-test_that("pre analyze words df", {
-  skip_if_offline()
-  if (!model_works("small"))
-    get_model("small")
-
   anl <- tibble::tibble(
     morphs = c("팅기", "었", "어"),
     pos = c("vv", "ep", "ef"),
