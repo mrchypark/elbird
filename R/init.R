ff714dd1a8e469edb49d3f54056eb8e196548a96
chanyub Park <mrchypark@gmail.com>
2022-06-18 00:48:42 +0900

    add docs for init

--- R/init.R
+++ R/init.R
@@ -2,6 +2,13 @@ init_chk_not <- function() {
   length(ls(envir = .el)) != 1
 }
 
+#' Initialize model for function api
+#'
+#' @param num_workers \code{int(optional)}: use multi-thread core number. default is 0 which means use all core.
+#' @param model_size \code{char(optional)}: kiwi model select. default is "base". "small", "large" is available.
+#' @param integrate_allomorph \code{bool(optional)}: default is TRUE.
+#' @param load_default_dict \code{bool(optional)}: use defualt dictionary. default is TRUE.
+#' @export
 init <- function(num_workers = 0,
                  model_size = "base",
                  integrate_allomorph = TRUE,
