c477635794aec6c6895c0d6a45d1cd547946415a
chanyub Park <mrchypark@gmail.com>
2022-08-04 14:11:50 +0900

    model version sync

--- R/init.R
+++ R/init.R
@@ -2,7 +2,7 @@ init_chk_not <- function() {
   length(ls(envir = .el)) != 1
 }
 
-init <- function(size = "small") {
+init <- function(size = "base") {
   if (!kiwi_model_exists(size))
     get_kiwi_models(size)
 
