92f0b0676423860e5a79658c66ccd08149dd5d6d
chanyub Park <mrchypark@gmail.com>
2022-08-04 16:00:25 +0900

    change build toolchain

--- tools/winbuild.sh
+++ tools/winbuild.sh
@@ -2,6 +2,11 @@
 ## https://github.com/r-windows/rtools-packages#extra-build-utilities-ie-autotools
 ##
 
+pacman -Syuu
+pacman -S cmake
+export PATH=/x86_64-w64-mingw32.static.posix/bin/:$PATH
+
+
 git clone -b v0.11.2 --depth 1 https://github.com/bab2min/Kiwi
 cd Kiwi
 
@@ -16,6 +21,6 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DKIWI_BUILD_TEST=OFF -DKIWI_USE_CPUINFO=OFF -DKIWI_USE_MIMALLOC=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON ..
 cmake -DCMAKE_BUILD_TYPE=Release -DKIWI_BUILD_TEST=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON ..
 make kiwi_static
-# cp libkiwi_static.a ../../kiwi-build/lib/i386/libkiwi.a
-# cp libkiwi_static.a ../../kiwi-build/lib/x64/libkiwi.a
-# cp libkiwi_static.a ../../kiwi-build/lib/x64-ucrt/libkiwi.a
+cp libkiwi_static.a ../../kiwi-build/lib/i386/libkiwi.a
+cp libkiwi_static.a ../../kiwi-build/lib/x64/libkiwi.a
+cp libkiwi_static.a ../../kiwi-build/lib/x64-ucrt/libkiwi.a
