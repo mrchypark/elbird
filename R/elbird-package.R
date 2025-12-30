eabd4ab7d9168db215ff7361bd457cd493346f53
Chanyub.Park <mrchypark@gmail.com>
2025-12-30 20:32:28 +0900

    Restore useDynLib and keep kiwi.dll in libs

--- R/elbird-package.R
+++ R/elbird-package.R
@@ -4,6 +4,7 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @useDynLib elbird, .registration = TRUE
 #' @importFrom tibble tibble
 ## usethis namespace: end
 NULL
